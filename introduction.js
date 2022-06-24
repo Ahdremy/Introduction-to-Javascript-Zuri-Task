23ee8ebe0dbad00c1c0896b561a69caed9ac965e
Ahdremy <workwithadremy@gmail.com>
2022-06-24 14:46:04 +0100

    first commit

---
+++ introduction.js
@@ -0,0 +1,11 @@
+alert('My name is Anietie');
+alert('5.8ft');
+alert('Nigeria');
+
+var name = ('My name is Anietie David');
+var height = ('5.7ft');
+var country = ('Nigeria');
+
+console.log(name);
+console.log(height);
+console.log(country);
